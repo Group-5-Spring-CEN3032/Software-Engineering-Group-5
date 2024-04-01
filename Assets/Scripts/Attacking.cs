337b328bca8dea45adfed93c883c177b607e1622
squaredphoenix <nixeriser@gmail.com>
2024-04-01 12:50:27 -0500

    NPC health bar face player and you can toggle the heartbar of NPC

---
+++ Assets/Scripts/Attacking.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attacking : MonoBehaviour
+{
+    [SerializeField]
+    private Stats stats;
+    [SerializeField]
+    private GameObject projectile;
+    [SerializeField]
+    private Transform firePosition;
+    [SerializeField]
+    private float speed;
+    [SerializeField]
+    private float targetTime;
+    private float time;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        time = targetTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        targetTime -= Time.deltaTime;
+
+        if (targetTime <= 0.0f)
+        {
+            if (Input.GetMouseButtonDown(1))
+            {
+                LaunchProjectile();
+                targetTime = time;
+            }
+        }
+    }
+
+    void LaunchProjectile()
+    {
+        var bullet = Instantiate(projectile, firePosition.position, firePosition.rotation);
+        bullet.GetComponent<projectile>().SetDamage(stats.AttackPower);
+        bullet.GetComponent<Rigidbody>().velocity = firePosition.forward * speed;
+    }
+}
