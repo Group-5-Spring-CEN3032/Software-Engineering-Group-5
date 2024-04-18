2869902484d01a74219554c57db5fd1c432b75de
Evan Overly <eho1@students.uwf.edu>
2024-04-18 15:34:31 -0500

    Removed unused import to resolve errors

--- Assets/Scripts/Persistence/PersistentClass.cs
+++ Assets/Scripts/Persistence/PersistentClass.cs
@@ -1,12 +1,6 @@
-using Palmmedia.ReportGenerator.Core.Common;
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using System.IO;
-using System.Text;
 using UnityEngine;
-using UnityEngine.UIElements;
-using UnityEngine.Windows;
+
 
 [ExecuteInEditMode]
 public abstract class PersistentClass<TargetType> : MonoBehaviour
